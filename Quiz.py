ff4d071dd28c14796206b6b519dd8f2f657d5879
lokeshwari5670-lgtm <lavany5670@gmail.com>
2026-08-24 13:10:46 +0530

    Create Quiz game

---
+++ Quiz.py
@@ -0,0 +1,9 @@
+print("🎮 Welcome to my quiz game")
+print("🎮 Welcome to My Quiz Game!")
+
+answer = input("What is the capital of India? ")
+
+if answer.lower() == "delhi":
+    print("✅ Correct!")
+else:
+    print("❌ Wrong answer!")
