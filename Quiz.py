b5e71a64df357d98c541ef22c9c0b11e077e9edb
lokeshwari5670-lgtm <lavany5670@gmail.com>
2026-08-25 22:31:18 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -7,3 +7,16 @@ if answer.lower() == "delhi":
     print("✅ Correct!")
 else:
     print("❌ Wrong answer!")
+print("🎮 Welcome to My Quiz Game!")
+
+score = 0
+
+answer = input("What is the capital of India? ")
+
+if answer.lower() == "delhi":
+    print("✅ Correct!")
+    score = score + 1
+else:
+    print("❌ Wrong answer!")
+
+print("Your score is:", score)
